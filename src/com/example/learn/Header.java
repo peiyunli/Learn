f80e16bc39205ae1794b629362424127b33da056
peiyunli <peiyunli@pc4.home>
2014-06-02 14:27:14 +0200

    header

---
+++ src/com/example/learn/Header.java
@@ -0,0 +1,14 @@
+package com.example.learn;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+
+public class Header extends Panel {
+	
+	public Header(){
+		
+	
+        Label head=new Label("Evaluation des competeces");
+        this.addComponent(head);
+}
+}
